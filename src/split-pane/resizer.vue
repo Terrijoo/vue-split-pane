e8c9e65409e1b0a8aa105007c8c861d239177c41
Sören Penter <soeren.penter89@gmail.com>
2018-12-12 16:58:52 +0100

    Style adjustments, gonna need to test

--- src/split-pane/resizer.vue
+++ src/split-pane/resizer.vue
@@ -23,34 +23,67 @@
 </script>
 
 <style scoped>
-.splitter-pane-resizer {
-  -moz-box-sizing: border-box;
-	-webkit-box-sizing: border-box;
-	box-sizing: border-box;
-	background: #000;
-	position: absolute;
-	opacity: .2;
-	z-index: 1;
-	-moz-background-clip: padding;
-	-webkit-background-clip: padding;
-	background-clip: padding-box;
-}
+	.splitter-pane-resizer {
+		-moz-box-sizing: border-box;
+		-webkit-box-sizing: border-box;
+		box-sizing: border-box;
+		background: #000;
+		position: absolute;
+		z-index: 1;
+		-moz-background-clip: padding;
+		-webkit-background-clip: padding;
+		background-clip: padding-box;
+		margin: 0;
+		left: 0;
+		opacity: 1 !important;
+	}
 
-.splitter-pane-resizer.horizontal {
-  height: 11px;
-	margin: -5px 0;
-	border-top: 5px solid rgba(255, 255, 255, 0);
-	border-bottom: 5px solid rgba(255, 255, 255, 0);
-	cursor: row-resize;
-	width: 100%;
-}
+	.splitter-pane-resizer.horizontal {
+		margin: -5px 0;
+		cursor: row-resize;
+		width: 100%;
+		height: 6px;
+		background: #eee;
+		border-top: 1px solid #ddd !important;
+		border-bottom: 1px solid #ddd !important;
+	}
 
-.splitter-pane-resizer.vertical {
-  width: 11px;
-	height: 100%;
-	margin-left: -5px;
-	border-left: 5px solid rgba(255, 255, 255, 0);
-	border-right: 5px solid rgba(255, 255, 255, 0);
-	cursor: col-resize;
-}
+	.splitter-pane-resizer.vertical {
+		height: 100%;
+		cursor: col-resize;
+		width: 6px;
+		background: #eee;
+		border-left: 1px solid #ddd !important;
+		border-right: 1px solid #ddd !important;
+	}
+
+	.splitter-pane-resizer.horizontal:before {
+		display: block;
+		content: "";
+		height: 3px;
+		width: 40px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin-left: -20px;
+		margin-top: -2px;
+		border: 1px solid #ccc;
+		border-radius: 3px;
+		background: #f8f8f8;
+	}
+
+	.splitter-pane-resizer.vertical:before {
+		display: block;
+		content: "";
+		height: 40px;
+		width: 3px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin-left: -3px;
+		margin-top: -2px;
+		border: 1px solid #ccc;
+		border-radius: 3px;
+		background: #f8f8f8;
+	}
 </style>
